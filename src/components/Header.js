0831f74d9d68bd8a1328b0133e17c5ae772888d4
Shawn Meyer <srbmwins@gmail.com>
2018-12-21 15:28:56 -0800

    header set up

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MainNav from './MainNav';
+
+const Header = () => {
+  return (
+    <header>
+      <h1> Portland Area Adventure Conditions</h1>
+      <div>
+        <MainNav />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/MainNav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MainNav = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a>Mt Hood</a></li>
+        <li><a>The Gorge</a></li>
+        <li><a>Mt St Helens</a></li>
+        <li><a>The Coast</a></li>
+        <li><a>Coastal Range</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default MainNav;

da3062df70407dd656b1884045a4e8d199e66d88
Shawn Meyer <srbmwins@gmail.com>
2018-12-23 13:44:42 -0800

    Initialized Home component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Home from './Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <Route exact path="/" component={Home} />
+        </div>
+    </BrowserRouter>
     );
   }
 }
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
+import MainNav from './components/MainNav';
+
+const Home = () => {
+  return (
+    <Jumbotron>
+      <h1>Where's Your Next Adventure</h1>
+      <p>Select the area near Portland where you are going to see what the weather is like.</p>
+      <MainNav />
+    </Jumbotron>
+  );
+}
+
+export default Home;
